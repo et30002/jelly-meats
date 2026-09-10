e4b431d11f2d6ac65fb2c2642a454b900be9bd68
Elizabeth <ext220019@utdallas.edu>
2026-09-10 10:43:11 -0500

    Added Quit Function

---
+++ scripts/main.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	# Start level.
+	if Input.is_action_just_pressed("jump"):
+		get_tree().change_scene_to_file('res://scenes/level_1.tscn')
+	
+	# Quit Game.
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
