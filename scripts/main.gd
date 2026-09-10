248b33bc165cc3a29aec3f7623270c5618c844b0
Elizabeth <ext220019@utdallas.edu>
2026-09-10 13:05:24 -0500

    Cleaned Menu, Added Menu Function, and Started Health Bar Scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+
+@onready var fullscreen: Button = $Buttons/Fullscreen
+@onready var windowed: Button = $Windowed
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,6 +17,19 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		get_tree().change_scene_to_file('res://scenes/level_1.tscn')
 	
-	# Quit Game.
+	# Quit game.
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+# Volume slider.
+
+# Fullscreen.
+func _on_fullscreen_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _on_windowed_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+# Click on quit button.
+func _on_quit_pressed() -> void:
+	get_tree().quit()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 350.0
 const JUMP_VELOCITY = -700.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
