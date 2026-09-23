436caeb8eeb23b4a10ce6781ba513ccdb96c7154
Elizabeth <ext220019@utdallas.edu>
2026-09-23 14:06:49 -0500

    Added Temp Boss Sprite & Created Standard Platofrms.

--- scripts/boss_1.gd
+++ scripts/boss_1.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
-@onready var progress_bar: ProgressBar = $ProgressBar
-@onready var color_rect: ColorRect = $ColorRect
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,13 +10,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	
-	#Lower boss health.
-	if Input.is_action_just_pressed("attack"):
-		progress_bar.set_value_no_signal(progress_bar.value - 10)
-	
-	# If player wins.
-	if progress_bar.value == 0:
-		progress_bar.visible = false
-		color_rect.visible = false
-	
+	# Play boss idle.
+	animated_sprite.play("idle")
