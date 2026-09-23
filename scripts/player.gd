2b94524990c88596591e4e68f6dca2a75705ec53
Elizabeth <ext220019@utdallas.edu>
2026-09-23 00:06:57 -0500

    Fixed Attack Animation & Updated Sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
+const SPEED = 600
+const JUMP_VELOCITY = -1000
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var knife_collision: CollisionShape2D = $AnimatedSprite2D/KnifeHit/KnifeCollision
 
 
-
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -20,21 +21,27 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	
+	
+	
 	# Flip the sprite.
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
+	
 	# Play animations.
-	# Enable attack collision box.
-	if Input.is_action_just_pressed("attack"):
-		knife_collision.disabled = false
-		animated_sprite.play("attack")
-		await animated_sprite.animation_finished
-	elif direction == 0:
-		knife_collision.disabled = true
-		animated_sprite.play("idle")
+	if not (animated_sprite.is_playing() and animated_sprite.animation == "attack"):
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("walk")
+		else:
+			animated_sprite.play("jump")
+	
+		if Input.is_action_just_pressed("attack"):
+			animated_sprite.play("attack")
 	
 	# Move the sprite.
 	if direction:
